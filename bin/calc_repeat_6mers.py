c2f308cff7973ec5e56d676fdf70748e7aab8a01
Felix Gabler <felix.gabler98@gmail.com>
2022-04-12 14:29:46 +0200

    calculate frequency of 2000 most frequent 6mers

--- bin/calc_repeat_6mers.py
+++ bin/calc_repeat_6mers.py
@@ -2,24 +2,52 @@
 
 import glob
 import re
+from collections import Counter
+
 import pandas as pd
 from pqdm.threads import pqdm
 from tqdm.auto import tqdm
 
 from iupred3.iupred3_lib import read_seq
 
-data_dir = '/tmp/global2/vikram/felix/master_thesis/data/alphafold/v2'
+data_dir = '/tmp/global2/vikram/felix/master_thesis/data'
+af_data_dir = f'{data_dir}/alphafold/v2'
+already_finished_list_file = f"{af_data_dir}/proteomes_with_6mer_counts_2000.csv"
 
-repeats_regex = '(?=(XXXXXX|QQQQQQ|SSSSSS|AAAAAA|GGGGGG|PPPPPP|EEEEEE|LLLLSL|NNNNNN|DDDDDD|TTTTTT|KKKKKK))'
-non_repeats_regex = '(?=(GGFGNW|DMAFPR|TVYPPL|WTVYPP|GWTVYP|PDMAFP|GFGNWL|LLLSLP|NNMSFW|SLPVLA|GTGWTV|TGWTVY|LSLPVL|IGGFGN|LLSLPV|FFMVMP|MIFFMV|IFFMVM|IFSLHL|SLHLAG|APDMAF|FSLHLA|LPVLAG|SFWLLP|FPRMNN|FWLLPP|VTAHAF|PVLAGA|SSILGA|VLAGAI|NMSFWL|MNNMSF|AFPRMN|GAPDMA|RMNNMS|LAGAIT|MSFWLL|MAFPRM|PRMNNM|AGAITM|FMVMPI|ITMLLT|VYPPLS|WLLPPS|TMLLTD|GAITML|AITMLL|MIGGFG|IVTAHA|GNWLVP|FGNWLV|GAGTGW|AGTGWT|MLLTDR|AIFSLH|NWLVPL|AGISSI|WLVPLM|MVMPIM|LLTDRN|LGAPDM|MLGAPD|GSGKST|GISSIL|PIMIGG|ISSILG|IMIGGF|YPPLSS|MPIMIG|VMPIMI|LAIFSL|LLPPSL|DLAIFS|PLMLGA|LMLGAP|VDLAIF|HTGEKP|FIMIFF|AFIMIF|HAFIMI|IMIFFM|SVDLAI|LAGISS|LHLAGI|HLAGIS|YNVIVT|LVPLML|PLFVWS))'
+df_counts = pd.read_csv(f'{data_dir}/uniprot/trembl_kmer_counts.csv', index_col=None).sort_values('count',
+                                                                                                  ascending=False)
+df_counts = df_counts[df_counts['kmer'] != 'XXXXXX']
+
+
+def check_repeat(kmer: str, min_len=5):
+    return any(all(c == kmer[start] for c in kmer[start + 1:start + min_len]) for start in range(len(kmer) - min_len))
+
+
+most_freq_kmers = df_counts['kmer'][:2000].values
+repeats = []
+non_repeats = []
+for kmer in most_freq_kmers:
+    if check_repeat(kmer):
+        repeats.append(kmer)
+    else:
+        non_repeats.append(kmer)
 
 
 def get_sequence_from_file(uniprot_id: str):
     try:
-        return read_seq(f'{data_dir}/sequences/{uniprot_id}.fasta')
+        return read_seq(f'{af_data_dir}/sequences/{uniprot_id}.fasta')
     except Exception as e:
         print(f'Failed reading fasta file {uniprot_id}: {e.message if hasattr(e, "message") else e}')
     return None
+
+
+def count_6mers_counter(seq: str, k=6):
+    counter = Counter()
+    n_kmers = len(seq) - k + 1
+    for i in range(n_kmers):
+        kmer = seq[i:i + k]
+        counter[kmer] += 1
+    return sum([counter[repeat] for repeat in repeats]), sum([counter[non_repeat] for non_repeat in non_repeats])
 
 
 def load_6mer_counts(uniprot_id: str):
@@ -27,7 +55,7 @@ def load_6mer_counts(uniprot_id: str):
     if seq is None:
         return None, None
     try:
-        return sum(1 for _ in re.finditer(repeats_regex, seq)), sum(1 for _ in re.finditer(non_repeats_regex, seq))
+        return count_6mers_counter(seq)
     except:
         print(f'Error in {seq}')
     return None, None
@@ -40,39 +68,38 @@ def extend_proteome_features_with_6mer_counts(proteome_file: str):
     except pd.errors.EmptyDataError:
         print(f'Could not read file {proteome_file}')
         return
-    if 'repeat_6mers_100' not in df_proteome:
-        df_proteome['repeat_6mers_100'] = None
-    if 'non_repeat_6mers_100' not in df_proteome:
-        df_proteome['non_repeat_6mers_100'] = None
-    df_proteome['repeat_6mers_100'] = pd.to_numeric(df_proteome['repeat_6mers_100'], errors='coerce')
-    df_proteome['non_repeat_6mers_100'] = pd.to_numeric(df_proteome['non_repeat_6mers_100'], errors='coerce')
-    to_fill = df_proteome[df_proteome['repeat_6mers_100'].isnull()].index
+    if 'repeat_6mers_2000' not in df_proteome:
+        df_proteome['repeat_6mers_2000'] = None
+    if 'non_repeat_6mers_2000' not in df_proteome:
+        df_proteome['non_repeat_6mers_2000'] = None
+    df_proteome['repeat_6mers_2000'] = pd.to_numeric(df_proteome['repeat_6mers_2000'], errors='coerce')
+    df_proteome['non_repeat_6mers_2000'] = pd.to_numeric(df_proteome['non_repeat_6mers_2000'], errors='coerce')
+    to_fill = df_proteome[df_proteome['repeat_6mers_2000'].isnull()].index
     if len(to_fill) == 0:
-        proteomes_with_6mer_counts = pd.read_csv(f"{data_dir}/proteomes_with_6mer_counts.csv", header=None)
-        proteomes_with_6mer_counts.loc[len(proteomes_with_6mer_counts.index)] = [proteome]
-        proteomes_with_6mer_counts.to_csv(f'{data_dir}/proteomes_with_6mer_counts.csv', header=False,
-                                          index=False)
-        print('Already filled with counted 6mers')
+        proteomes_already_finished = pd.read_csv(already_finished_list_file)
+        proteomes_already_finished.loc[len(proteomes_already_finished.index)] = [proteome]
+        proteomes_already_finished.to_csv(already_finished_list_file, index=False)
+        print('Already filled')
         return
     counts = pqdm(df_proteome['uniprot_id'][to_fill], load_6mer_counts, n_jobs=20, desc=f'Sequences in {proteome}')
-    df_proteome.loc[to_fill, ['repeat_6mers_100', 'non_repeat_6mers_100']] = counts
-    df_proteome.to_csv(f'{data_dir}/AA_scores/{proteome}.csv')
+    df_proteome.loc[to_fill, ['repeat_6mers_2000', 'non_repeat_6mers_2000']] = counts
+    df_proteome.to_csv(f'{af_data_dir}/AA_scores/{proteome}.csv')
     del df_proteome
     return
 
 
 def calculate_6mer_counts():
-    proteomes_with_domain_count = pd.read_csv(f"{data_dir}/proteomes_with_domain_count.csv", header=None)
-    proteomes_with_domain_count_list = proteomes_with_domain_count.squeeze('columns').to_list()
-    proteomes_with_6mer_counts = pd.read_csv(f"{data_dir}/proteomes_with_6mer_counts.csv", header=None)
-    proteomes_with_6mer_counts_list = proteomes_with_6mer_counts.squeeze('columns').to_list()
-    proteomes_to_extend_with_6mer_counts = set(proteomes_with_domain_count_list).difference(
-        set(proteomes_with_6mer_counts_list))
-    all_proteome_files = glob.glob(f"{data_dir}/AA_scores/*.csv")
-    proteome_files_without_6mer_counts = list(
-        filter(lambda file: re.search(r"/([A-Z0-9]+).csv", file).group(1) in proteomes_to_extend_with_6mer_counts,
+    proteomes_ready = pd.read_csv(f"{af_data_dir}/proteomes_with_domain_count.csv", header=None)
+    proteomes_ready_list = proteomes_ready.squeeze('columns').to_list()
+    proteomes_already_finished = pd.read_csv(already_finished_list_file)
+    proteomes_already_finished_list = proteomes_already_finished.squeeze('columns').to_list()
+    proteomes_to_process = set(proteomes_ready_list).difference(
+        set(proteomes_already_finished_list))
+    all_proteome_files = glob.glob(f"{af_data_dir}/AA_scores/*.csv")
+    files_to_process = list(
+        filter(lambda file: re.search(r"/([A-Z0-9]+).csv", file).group(1) in proteomes_to_process,
                all_proteome_files))
-    for file in tqdm(proteome_files_without_6mer_counts, desc='Files'):
+    for file in tqdm(files_to_process, desc='Files'):
         extend_proteome_features_with_6mer_counts(file)
     return
 
--- bin/run_script.sh
+++ bin/run_script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-python /ebio/abt1/fgabler/master_thesis/bin/calc_iupred_plddt_spearman.py
+python /ebio/abt1/fgabler/master_thesis/bin/calc_repeat_6mers.py
