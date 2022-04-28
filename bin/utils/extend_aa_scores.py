78cbc2483701638ba8a4c6d5c514df03a87e81c8
Felix Gabler <felix.gabler98@gmail.com>
2022-04-28 15:38:34 +0200

    compare go functions and process for correlated vs uncorrelated and positively correlated vs all

--- bin/utils/extend_aa_scores.py
+++ bin/utils/extend_aa_scores.py
@@ -4,6 +4,7 @@ import re
 import pandas as pd
 from pqdm.threads import pqdm
 from tqdm.auto import tqdm
+from os.path import exists
 
 
 def read_seq(fasta_file):
@@ -24,7 +25,7 @@ def get_sequence_from_file(sequences_folder: str, uniprot_id: str):
     return None
 
 
-def extend_proteome_features_with_new_property(fun, filter_sequences, proteome_file: str, prop: str,
+def extend_proteome_features_with_new_property(fun, filter_sequences, proteome_file: str, props: [str],
                                                already_finished_file: str, result_folder: str):
     proteome = re.search(r"/([A-Z0-9]+).csv", proteome_file).group(1)
     try:
@@ -34,27 +35,36 @@ def extend_proteome_features_with_new_property(fun, filter_sequences, proteome_f
         return
     if filter_sequences is not None:
         df_proteome = filter_sequences(df_proteome)
-    if prop not in df_proteome:
-        df_proteome[prop] = None
-    df_proteome[prop] = pd.to_numeric(df_proteome[prop], errors='coerce')
-    to_fill = df_proteome[df_proteome[prop].isnull()].index
+    for prop in props:
+        if prop not in df_proteome:
+            df_proteome[prop] = None
+        df_proteome[prop] = pd.to_numeric(df_proteome[prop], errors='coerce')
+    to_fill = df_proteome[df_proteome[props[0]].isnull()].index
     if len(to_fill) == 0:
         proteomes_already_finished = pd.read_csv(already_finished_file)
         proteomes_already_finished.loc[len(proteomes_already_finished.index)] = [proteome]
         proteomes_already_finished.to_csv(already_finished_file, index=False)
         print('Already finished file')
         return
-    df_proteome.loc[to_fill, prop] = pqdm(df_proteome['uniprot_id'][to_fill], fun, n_jobs=20,
-                                          desc=f'Sequences in {proteome}')
+    df_proteome.loc[to_fill, props] = pqdm(df_proteome['uniprot_id'][to_fill], fun, n_jobs=20,
+                                           desc=f'Sequences in {proteome}')
     df_proteome.to_csv(f'{result_folder}/{proteome}.csv')
     del df_proteome
     return
 
 
-def calculate_new_property_for_proteomes(fun, prop: str, ready_file: str, already_finished_file: str,
-                                         result_folder: str, filter_sequences=None):
+def calculate_new_property_for_proteomes(fun,
+                                         ready_file: str,
+                                         already_finished_file: str,
+                                         result_folder: str,
+                                         props: [str] = None,
+                                         prop: str = None,
+                                         filter_sequences=None):
     proteomes_ready = pd.read_csv(ready_file)
     proteomes_ready_list = proteomes_ready.squeeze('columns').to_list()
+    if not exists(already_finished_file):
+        with open(already_finished_file, 'w') as file:
+            file.write('proteome\n')
     proteomes_already_finished = pd.read_csv(already_finished_file)
     proteomes_already_finished_list = proteomes_already_finished.squeeze('columns').to_list()
     proteomes_to_process = set(proteomes_ready_list).difference(
@@ -69,7 +79,7 @@ def calculate_new_property_for_proteomes(fun, prop: str, ready_file: str, alread
             fun,
             filter_sequences,
             file,
-            prop,
+            props if props is not None else [prop],
             already_finished_file,
             result_folder
         )
